78a4fbafb39786ecfd2521892eec35b4b7ba21bd
Arthur276 <arthur.cott@free.fr>
2022-02-14 21:36:12 +0100

    Add main_discordbot file

---
+++ main_discordbot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import mod_anime.py as anipy
+
+bot = commands.Bot(command_prefix = "!", description = "AnimeTime discord bot, devloped by Arthur")
+main_channel = "942152881996443708"
+
+'''@bot.event
+async def on_ready():
+    print("AnimeTime is ready !")
+    await main_channel.send("AnimeTime is ready !")
+
+bot.run("OTE2Njc0NzAwNDA1NTM4ODY4.Yatl1w.AEXrtRr9kVyyMFoeYHZcfr_tD_I")
+@bot.command()
+async def ajouter_anime(ctx):
+    await
+'''
