33199bdaf4a2ec6fddbbcba5830059bef99ea136
Arthur276 <arthur.cott@free.fr>
2022-08-15 16:58:34 +0200

    Corrections : -Certains attributs d'instances on été supprimés (ex: Anime.nb_saisons -> len(dict_saisons). -Les fonctions d'affichages ont été raccourcies en se basant sur la méthode .export_dict -L'attribut nb_anime devient id_anime, et ne peut être ré-attribué, autrefois possible et à l'origine de bugs. -Dictionnaire Anime.episodes_par_saison suprimmé car inutile et facilement remplaçable (dict_saison.nb_episodes). -Init = True est désormais la valeur par défaut lors de l'appel de la fonction init_episodes dans la fonction  ajouter_saison -Harmonisation des variables contenant les emplacements mémoires des animés (-> anime_id_memoire)

--- main_anime.py
+++ main_anime.py
@@ -27,8 +27,6 @@ class Saison():
             self.numero_saison = numero_saison
             self.dict_episodes = {}
             anime_id_memoire.dict_saisons[numero_saison] = self
-            anime_id_memoire.nb_saisons += 1
-            anime_id_memoire.nb_episodes += nb_episodes
             if init:
                 self.init_episodes()
             print(f"La saison {numero_saison} de {anime_id_memoire.nom_complet} a été ajouté")
@@ -49,14 +47,12 @@ class Saison():
     def afficher_episodes_saison(self):
         # STATUS : OK
         """Affiche les episode d'une saison"""
-        list_episodes_saison = {}
-        for episode in self.dict_episodes.keys():
-            list_episodes_saison[episode] = self.dict_episodes[episode]
-
+        dict_anime = self.anime_id_memoire.export_dict()
+        return dict_anime[animedata["dict_episodes_clé"]][str(self.numero_saison)]
 
 class Anime():
     instances_anime = {}
-    nb_anime = 0
+    id_anime = 0
 
     def __new__(cls, nom_complet):
         instances = cls.instances_anime
@@ -69,12 +65,9 @@ class Anime():
 
     def __init__(self, nom_complet):
         self.nom_complet = nom_complet
-        self.nb_saisons = 0
-        self.nb_episodes = 0
-        self.episodes_par_saison = {}
         self.dict_saisons = {}
-        self.id_local = Anime.nb_anime
-        Anime.nb_anime += 1
+        self.id_local = Anime.id_anime
+        Anime.id_anime += 1
 
 
     @classmethod
@@ -82,7 +75,7 @@ class Anime():
         # STATUS : OK
         """Ajoute un animé"""
         if not en_ligne:
-            globals()[f"anime_{Anime.nb_anime-1}"] = Anime(nom_anime)
+            globals()[f"anime_{Anime.id_anime-1}"] = Anime(nom_anime)
         else:
             maj_anime_liste()
             charger_anime(nom_anime,maj = False)
@@ -92,7 +85,6 @@ class Anime():
         # STATUS : OK
         """Supprime un animé"""
         del Anime.instances_anime[self.nom_complet]
-        Anime.nb_anime -= 1
         print(f"L'animé {self.nom_complet} à été supprimé")
 
     @classmethod
@@ -106,38 +98,22 @@ class Anime():
         return list_anime
 
 
-    def ajouter_saison(self,numero_saison,nb_episodes,init = True):
+    def ajouter_saison(self,numero_saison,nb_episodes):
         # STATUS : OK
         """Ajoute une saison a un animé"""
-        globals()[f"saison_{self.id_local}_{numero_saison}"] = Saison(self,numero_saison,nb_episodes, init = init)
+        globals()[f"saison_{self.id_local}_{numero_saison}"] = Saison(self,numero_saison,nb_episodes, init = True)
 
     def supprimer_saison(self,numero_saison):
         # STATUS : OK
         """Supprime une saison d'un animé"""
-        self.nb_saisons -= 1
-        self.nb_episodes -= self.dict_saisons[str(numero_saison)].nb_episodes
-        del self.dict_saisons[str(numero_saison)]
+        del self.dict_saisons[numero_saison]
         print(f"La saison {numero_saison} de l'anime {self.nom_complet} et ses épisodes ont été supprimés")
 
-    def afficher_saisons(self):
+    def afficher_saisons_episodes(self):
         # STATUS : OK
-        """Retourne un dictionnaire contenant les saisons d'un animé"""
-        affich_saisons = {}
-        for saison in self.dict_saisons.keys():
-            affich_saisons[saison] = self.dict_saisons[saison].nb_episodes
-        return affich_saisons
-
-    def afficher_episodes(self):
-        #STATUS : OK
-        """Retourne un dictionnaire contenant les noms d'épisode aux numéro d'épisodes associés"""
-        affich_episode = {}
-        for saison in self.dict_saisons.keys():
-            id_saison = self.dict_saisons[saison]
-            for episode in id_saison.dict_episodes.keys():
-                id_episode = id_saison.dict_episodes[episode]
-                titre = id_episode.nom_episode
-                affich_episode[f"S{saison}-E{episode}"] = id_episode.nom_episode
-        return affich_episode
+        """Retourne un dictionnaire contenant les saisons et ses épisodes d'un animé"""
+        dict_anime = self.export_dict()
+        return dict_anime[animedata["dict_episodes_clé"]]
 
     def export_dict(self):
         """Exporte toutes les données d'un animé vers un dictionnaire"""
@@ -177,10 +153,10 @@ def charger_anime(nom_anime,maj = False,local = False):
             if element["type"] == "anime" and nom_anime == element[animedata["dict_nom_anime_clé"]]:
                 if not maj:
                     Anime.ajouter_anime(nom_anime,en_ligne = False)
-                id_memoire_anime = Anime.instances_anime[nom_anime]
+                anime_id_memoire = Anime.instances_anime[nom_anime]
                 for saison in element[animedata["dict_episodes_clé"]].keys():
-                    id_memoire_anime.ajouter_saison(int(saison),len(element[animedata["dict_episodes_clé"]][saison]))
-                    id_memoire_saison = id_memoire_anime.dict_saisons[int(saison)]
+                    anime_id_memoire.ajouter_saison(int(saison),len(element[animedata["dict_episodes_clé"]][saison]))
+                    id_memoire_saison = anime_id_memoire.dict_saisons[int(saison)]
                     for episode in element[animedata["dict_episodes_clé"]][saison].keys():
                         id_memoire_saison.edit_nom_episode(int(episode),element[animedata["dict_episodes_clé"]][saison][episode])
                 print("L'animé a bien été chargé")
